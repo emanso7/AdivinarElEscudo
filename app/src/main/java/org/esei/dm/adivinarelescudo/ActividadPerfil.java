e39f9bd03e1dc72477ece1268ba911499e913f52
emanso7 <emiliomanso33@gmail.com>
2024-11-21 19:38:34 +0100

    add actividadPerfil vacia

--- app/src/main/java/org/esei/dm/adivinarelescudo/ActividadPerfil.java
+++ app/src/main/java/org/esei/dm/adivinarelescudo/ActividadPerfil.java
@@ -1,8 +1,14 @@
 package org.esei.dm.adivinarelescudo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,5 +26,26 @@ public class ActividadPerfil extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button btmenu = (Button) this.findViewById(R.id.menu_button);
+        btmenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // this.startActivity( new Intent( this, actividad_clasificacion.class ) );
+                Intent intent = new Intent(ActividadPerfil.this, ActividadHome.class);
+                startActivity(intent);
+            }
+        });
+        Button btedit=(Button) this.findViewById(R.id.editar_button);
+        btedit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //lanza actividad modificar datos(aun no creada)
+                /*Intent intent = new Intent(ActividadPerfil.this, ActividadModificarPerfil.class);
+                startActivity(intent);*/
+
+            }
+        });
     }
+
+
 }

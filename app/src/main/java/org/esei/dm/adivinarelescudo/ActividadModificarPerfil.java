29f5c7f6e2bd7ac96cb255f7e74312d0b6ae5db8
emanso7 <emiliomanso33@gmail.com>
2024-11-29 13:47:50 +0100

    actualizado layouts de modificar y ver perfil

--- app/src/main/java/org/esei/dm/adivinarelescudo/ActividadModificarPerfil.java
+++ app/src/main/java/org/esei/dm/adivinarelescudo/ActividadModificarPerfil.java
@@ -1,6 +1,9 @@
 package org.esei.dm.adivinarelescudo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,16 @@ public class ActividadModificarPerfil extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button btCancel=(Button) this.findViewById(R.id.cancel_button);
+        btCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //lanza actividad modificar datos(aun no creada)
+                Intent intent = new Intent(ActividadModificarPerfil.this, ActividadPerfil.class);
+                startActivity(intent);
+
+            }
         });
     }
 }

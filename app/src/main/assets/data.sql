5f9f770674230acd0ee453472d6fe983f90dbe96
emanso7 <emiliomanso33@gmail.com>
2024-12-09 21:47:03 +0100

    Update data.sql

--- app/src/main/assets/data.sql
+++ app/src/main/assets/data.sql
@@ -1,35 +1,61 @@
-INSERT INTO ADIVINAESCUDO_QUIZ_TABLE_NAME
-(ADIVINAESCUDO_QUIZ_COLUMN_ID,ADIVINAESCUDO_QUIZ_COLUMN_CORRECT, ADIVINAESCUDO_QUIZ_COLUMN_PHOTO, ADIVINAESCUDO_QUIZ_COLUMN_OP1,
- ADIVINAESCUDO_QUIZ_COLUMN_OP2, ADIVINAESCUDO_QUIZ_COLUMN_OP3, ADIVINAESCUDO_QUIZ_COLUMN_OP4,
-  ADIVINAESCUDO_QUIZ_COLUMN_DIFICULTAD) VALUES
-(1, 'escudo_nogueira.jpg', 'S.D. NOGUEIRA RAMUIN', 'SPORTING CARBALLIÑO', 'C.D. MUIÑOS', 'S.D. NOGUEIRA RAMUIN', 'C.D. MACEDA', 'Facil'),
-(2, 'escudo_carballiño.png', 'SPORTING CARBALLIÑO', 'C.D. ALLARIZ', 'SPORTING CARBALLIÑO', 'MONTERREY C.F', 'S.D. MELIAS', 'Facil'),
-(3, 'escudo_muiños.jpg', 'C.D. MUIÑOS', 'C.D. MUIÑOS', 'C.D. VELLE', 'C.D. SEIXALBO', 'C.D. VIANA', 'Facil'),
-(4, 'escudo_VELLE.jpg', 'C.D. VELLE', 'C.D. MUIÑOS', 'C.D. MACEDA', 'C.D. VELLE', 'MONTERREY C.F', 'Facil'),
-(5, 'escudo_allariz.jpg', 'C.D. ALLARIZ', 'C.D. VIANA', 'S.D. MELIAS', 'C.D. ALLARIZ', 'S.D. NOGUEIRA RAMUIN', 'Facil'),
-(6, 'escudo_maceda.jpg', 'C.D. MACEDA', 'C.D. MACEDA', 'MONTERREY C.F', 'SPORTING CARBALLIÑO', 'C.D. SEIXALBO', 'Facil'),
-(7, 'escudo_monterrey.jpg', 'MONTERREY C.F', 'MONTERREY C.F', 'C.D. ALLARIZ', 'C.D. VELLE', 'S.D. MELIAS', 'Facil'),
-(8, 'escudo_melias.jpg', 'S.D. MELIAS', 'C.D. VIANA', 'C.D. SEIXALBO', 'S.D. MELIAS', 'C.D. MUIÑOS', 'Facil'),
-(9, 'escudo_VIANA.jpg', 'C.D. VIANA', 'S.D. MELIAS', 'C.D. VIANA', 'C.D. MACEDA', 'SPORTING CARBALLIÑO', 'Facil'),
-(10, 'escudo_seixalbo.png', 'C.D. SEIXALBO', 'C.D. SEIXALBO', 'C.D. ALLARIZ', 'S.D. NOGUEIRA RAMUIN', 'MONTERREY C.F', 'Facil'),
-(11, 'escudo_coles.jpg', 'CD Coles', 'Francelos FC', 'CD Coles', 'Rayo 21', 'SCD Arrabaldo', 'Media'),
-(12, 'escudo_FRANCELOS.jpg', 'Francelos FC', 'CENTED B', 'Francelos FC', 'CD Rua', 'Antela FC B', 'Media'),
-(13, 'escudo_antela.jpeg', 'Antela FC B', 'UD Lobios', 'Rayo 21', 'Antela FC B', 'CD Coles', 'Media'),
-(14, 'escudo_atletico_vilariño.jpg', 'atletico vilariño', 'SCD Arrabaldo', 'atletico vilariño', 'SCD Paderne', 'CD Rua', 'Media'),
-(15, 'escudo_PADERNE.jpg', 'SCD Paderne', 'Francelos FC', 'CENTED B', 'SCD Paderne', 'UD Lobios', 'Media'),
-(16, 'escudo_CENTED.jpg', 'CENTED B', 'CENTED B', 'SCD Paderne', 'Antela FC B', 'Rayo 21', 'Media'),
-(17, 'escudo_RUA.jpg', 'CD Rua', 'CD Rua', 'atletico vilariño', 'SCD Arrabaldo', 'UD Lobios', 'Media'),
-(18, 'escudo_LOBIOS.jpg', 'UD Lobios', 'CD Coles', 'Francelos FC', 'UD Lobios', 'Rayo 21', 'Media'),
-(19, 'escudo_sdc_arrabaldo.jpg', 'SCD Arrabaldo', 'SCD Arrabaldo', 'CD Rua', 'Francelos FC', 'atletico vilariño', 'Media'),
-(20, 'escudo_rayo_21.png', 'Rayo 21', 'Antela FC B', 'CENTED B', 'Rayo 21', 'CD Coles', 'Media'),
-(21, 'escudo_pabellon.jpg', 'PABELLON OURENSE C.F.', 'PABELLON OURENSE C.F.', 'AMOEIRO C.F.', 'C.D. BARRA DE MIÑO', 'C.D. A BOLA', 'Dificil'),
-(22, 'escudo_AMOEIRO.png', 'AMOEIRO C.F.', 'C.D. VILAR DE BARRIO', 'AMOEIRO C.F.', 'SOUTOPENEDO C.F.', 'C.D. RANTE SAN CIBRAO', 'Dificil'),
-(23, 'escudo_salamonde.jpg', 'SALAMONDE C.F.', 'C.D. CEA', 'SALAMONDE C.F.', 'PADRENDA C.F.', 'C.D. A BOLA', 'Dificil'),
-(24, 'escudo_cea.jpg', 'C.D. CEA', 'C.D. CEA', 'AMOEIRO C.F.', 'C.D. RANTE SAN CIBRAO', 'C.D. BARRA DE MIÑO', 'Dificil'),
-(25, 'escudo_barrademiño.jpg', 'C.D. BARRA DE MIÑO', 'SALAMONDE C.F.', 'PADRENDA C.F.', 'C.D. BARRA DE MIÑO', 'SOUTOPENEDO C.F.', 'Dificil'),
-(26, 'escudo_SOUTOPENEDO.jpg', 'SOUTOPENEDO C.F.', 'SOUTOPENEDO C.F.', 'PABELLON OURENSE C.F.', 'C.D. VILAR DE BARRIO', 'C.D. CEA', 'Dificil'),
-(27, 'escudo_rante.jpg', 'C.D. RANTE SAN CIBRAO', 'C.D. BARRA DE MIÑO', 'PADRENDA C.F.', 'C.D. RANTE SAN CIBRAO', 'AMOEIRO C.F.', 'Dificil'),
-(28, 'vilar_de_barriojpg.jpg', 'C.D. VILAR DE BARRIO', 'C.D. A BOLA', 'C.D. CEA', 'C.D. VILAR DE BARRIO', 'PABELLON OURENSE C.F.', 'Dificil'),
-(29, 'escudo_a_bola.jpg', 'C.D. A BOLA', 'C.D. A BOLA', 'SALAMONDE C.F.', 'SOUTOPENEDO C.F.', 'AMOEIRO C.F.', 'Dificil'),
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(1, 'escudo_nogueira.jpg', 'S.D. NOGUEIRA RAMUIN', 'SPORTING CARBALLIÑO', 'C.D. MUIÑOS', 'S.D. NOGUEIRA RAMUIN', 'C.D. MACEDA', 'Facil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(2, 'escudo_carballiño.png', 'SPORTING CARBALLIÑO', 'C.D. ALLARIZ', 'SPORTING CARBALLIÑO', 'MONTERREY C.F', 'S.D. MELIAS', 'Facil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(3, 'escudo_muiños.jpg', 'C.D. MUIÑOS', 'C.D. MUIÑOS', 'C.D. VELLE', 'C.D. SEIXALBO', 'C.D. VIANA', 'Facil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(4, 'escudo_VELLE.jpg', 'C.D. VELLE', 'C.D. MUIÑOS', 'C.D. MACEDA', 'C.D. VELLE', 'MONTERREY C.F', 'Facil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(5, 'escudo_allariz.jpg', 'C.D. ALLARIZ', 'C.D. VIANA', 'S.D. MELIAS', 'C.D. ALLARIZ', 'S.D. NOGUEIRA RAMUIN', 'Facil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(6, 'escudo_maceda.jpg', 'C.D. MACEDA', 'C.D. MACEDA', 'MONTERREY C.F', 'SPORTING CARBALLIÑO', 'C.D. SEIXALBO', 'Facil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(7, 'escudo_monterrey.jpg', 'MONTERREY C.F', 'MONTERREY C.F', 'C.D. ALLARIZ', 'C.D. VELLE', 'S.D. MELIAS', 'Facil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(8, 'escudo_melias.jpg', 'S.D. MELIAS', 'C.D. VIANA', 'C.D. SEIXALBO', 'S.D. MELIAS', 'C.D. MUIÑOS', 'Facil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(9, 'escudo_VIANA.jpg', 'C.D. VIANA', 'S.D. MELIAS', 'C.D. VIANA', 'C.D. MACEDA', 'SPORTING CARBALLIÑO', 'Facil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(10, 'escudo_seixalbo.png', 'C.D. SEIXALBO', 'C.D. SEIXALBO', 'C.D. ALLARIZ', 'S.D. NOGUEIRA RAMUIN', 'MONTERREY C.F', 'Facil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(11, 'escudo_coles.jpg', 'CD Coles', 'Francelos FC', 'CD Coles', 'Rayo 21', 'SCD Arrabaldo', 'Media');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(12, 'escudo_FRANCELOS.jpg', 'Francelos FC', 'CENTED B', 'Francelos FC', 'CD Rua', 'Antela FC B', 'Media');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(13, 'escudo_antela.jpeg', 'Antela FC B', 'UD Lobios', 'Rayo 21', 'Antela FC B', 'CD Coles', 'Media');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(14, 'escudo_atletico_vilariño.jpg', 'atletico vilariño', 'SCD Arrabaldo', 'atletico vilariño', 'SCD Paderne', 'CD Rua', 'Media');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(15, 'escudo_PADERNE.jpg', 'SCD Paderne', 'Francelos FC', 'CENTED B', 'SCD Paderne', 'UD Lobios', 'Media');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(16, 'escudo_CENTED.jpg', 'CENTED B', 'CENTED B', 'SCD Paderne', 'Antela FC B', 'Rayo 21', 'Media');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(17, 'escudo_RUA.jpg', 'CD Rua', 'CD Rua', 'atletico vilariño', 'SCD Arrabaldo', 'UD Lobios', 'Media');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(18, 'escudo_LOBIOS.jpg', 'UD Lobios', 'CD Coles', 'Francelos FC', 'UD Lobios', 'Rayo 21', 'Media');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(19, 'escudo_sdc_arrabaldo.jpg', 'SCD Arrabaldo', 'SCD Arrabaldo', 'CD Rua', 'Francelos FC', 'atletico vilariño', 'Media');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(20, 'escudo_rayo_21.png', 'Rayo 21', 'Antela FC B', 'CENTED B', 'Rayo 21', 'CD Coles', 'Media');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(21, 'escudo_pabellon.jpg', 'PABELLON OURENSE C.F.', 'PABELLON OURENSE C.F.', 'AMOEIRO C.F.', 'C.D. BARRA DE MIÑO', 'C.D. A BOLA', 'Dificil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(22, 'escudo_AMOEIRO.png', 'AMOEIRO C.F.', 'C.D. VILAR DE BARRIO', 'AMOEIRO C.F.', 'SOUTOPENEDO C.F.', 'C.D. RANTE SAN CIBRAO', 'Dificil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(23, 'escudo_salamonde.jpg', 'SALAMONDE C.F.', 'C.D. CEA', 'SALAMONDE C.F.', 'PADRENDA C.F.', 'C.D. A BOLA', 'Dificil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(24, 'escudo_cea.jpg', 'C.D. CEA', 'C.D. CEA', 'AMOEIRO C.F.', 'C.D. RANTE SAN CIBRAO', 'C.D. BARRA DE MIÑO', 'Dificil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(25, 'escudo_barrademiño.jpg', 'C.D. BARRA DE MIÑO', 'SALAMONDE C.F.', 'PADRENDA C.F.', 'C.D. BARRA DE MIÑO', 'SOUTOPENEDO C.F.', 'Dificil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(26, 'escudo_SOUTOPENEDO.jpg', 'SOUTOPENEDO C.F.', 'SOUTOPENEDO C.F.', 'PABELLON OURENSE C.F.', 'C.D. VILAR DE BARRIO', 'C.D. CEA', 'Dificil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(27, 'escudo_rante.jpg', 'C.D. RANTE SAN CIBRAO', 'C.D. BARRA DE MIÑO', 'PADRENDA C.F.', 'C.D. RANTE SAN CIBRAO', 'AMOEIRO C.F.', 'Dificil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(28, 'vilar_de_barriojpg.jpg', 'C.D. VILAR DE BARRIO', 'C.D. A BOLA', 'C.D. CEA', 'C.D. VILAR DE BARRIO', 'PABELLON OURENSE C.F.', 'Dificil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
+(29, 'escudo_a_bola.jpg', 'C.D. A BOLA', 'C.D. A BOLA', 'SALAMONDE C.F.', 'SOUTOPENEDO C.F.', 'AMOEIRO C.F.', 'Dificil');
+INSERT INTO preguntas(_id, photo, correct, op1, op2, op3, op4,  dif) VALUES
 (30, 'escudo_PADRENDA.png', 'PADRENDA C.F.', 'PADRENDA C.F.', 'C.D. RANTE SAN CIBRAO', 'SALAMONDE C.F.', 'C.D. VILAR DE BARRIO', 'Dificil');
 
